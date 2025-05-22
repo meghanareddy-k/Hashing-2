8bad94cd43bf32af25cefff0012ad106f39a5d9a
Meghana reddy <kothakota.m@northeastern.edu>
2025-05-21 23:21:51 -0400

    completed hashing-2

--- Longest Palindrome.java
+++ Longest Palindrome.java
@@ -1,7 +1,24 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+//time: O(n)
+//Space:O(1)
+// create a set of char if the char is present, add in result +2 and remove the char as we already found the pair. If its occuring for the first time add in th set and in last if set is not empty then add +1 as we can have a Palindrome with odd length
+class Solution {
+    public int longestPalindrome(String s) {
+        HashSet<Character>sh = new HashSet<>();
+        int result=0;
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(sh.contains(ch)){
+                result= result+2;
+                sh.remove(ch);
+            }
+            else{
+                sh.add(ch);
+            }
 
-
-// Your code here along with comments explaining your approach
+        }
+        if(!sh.isEmpty()){
+            result= result+1;
+        }
+        return result;
+    }
+}
